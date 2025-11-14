5aeb10dbf7a4ac93e737eb3c3a200f266b604fc1
Daniel Ramos <daniel.ramosg@gmail.com>
2025-11-14 16:02:00 +0100

    Add German text

--- src/js/locales/de/de.ts
+++ src/js/locales/de/de.ts
@@ -10,6 +10,7 @@ const de = {
       ErrorOverEpoch: "Error over Epoch",
       VectorSpace: "Vector space",
       nnVisualization: "Neural Network visualization",
+      corpusText: `Ich suche die grünen Bücher im Wohnzimmer. Ich finde die gelben Servietten im Bad. Ich suche die blauen Vasen im Flur. Ich finde die grünen Servietten im Flur. Ich suche die blauen Bücher im Bad. Ich finde die gelben Vasen im Wohnzimmer. Ich suche die roten Kerzen im Schlafzimmer. Ich finde die grünen Kerzen im Bad. Ich suche die blauen Servietten im Wohnzimmer. Ich finde die gelben Bücher im Flur. Ich suche die roten Kerzen im Schlafzimmer. Ich finde die roten Vasen im Schlafzimmer.`,
     },
   },
 };
